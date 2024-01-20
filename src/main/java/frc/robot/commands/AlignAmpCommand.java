44434cc7160438df59e92be2dd8b8ea12088800a
MasonPaprzycki <93019015+MasonPaprzycki@users.noreply.github.com>
2024-01-20 13:06:56 -0700

    cleanup I forgot I didnt need those files

--- src/main/java/frc/robot/commands/AlignAmpCommand.java
+++ src/main/java/frc/robot/commands/AlignAmpCommand.java
@@ -3,7 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.vision.VisionSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 public class AlignAmpCommand extends InstantCommand {
 
