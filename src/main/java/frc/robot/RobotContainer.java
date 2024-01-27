68ae4aac2f146359d1bf55414668bb90251d6642
Ben Griffin <Bennett.Griffin@gmail.com>
2024-01-27 13:20:27 -0700

    Revert "configure errors going on did not have basic code  will need to pull off main when it gets put in"
    This reverts commit e1b36b1387057d3be57dd241e2db02b5f51f2190.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,68 +4,22 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.DriveConstants;
-import frc.robot.constants.DriveConstants.SpeedMode;
-import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class RobotContainer {
   private boolean isTeleop;
-  private DriveSubsystem driveSubsystem;
 
   private SendableChooser<Command> autoCommandChooser = new SendableChooser<>();
-  private final CommandXboxController driverController =
-      new CommandXboxController(DriveConstants.DRIVER_CONTROLLER_PORT);
 
   public RobotContainer() {
-    driveSubsystem = new DriveSubsystem();
     configureBindings();
     buildAutoList();
   }
 
-  private RunCommand getDefaultCommand() {
-    return new RunCommand(
-        () ->
-            driveSubsystem.drive(
-                -MathUtil.applyDeadband(driverController.getLeftY(), 0.1),
-                -MathUtil.applyDeadband(driverController.getLeftX(), 0.1),
-                -MathUtil.applyDeadband(driverController.getRightX(), 0.1),
-                driveSubsystem.getFieldRelative()),
-        driveSubsystem);
-  }
-
-  private void configureBindings() {
-    configureDriverController();
-  }
-
-  private void configureDriverController() {
-    driverController.a().onTrue(new InstantCommand(driveSubsystem::zeroHeading));
-
-    driverController
-        .b()
-        .onTrue(new InstantCommand(() -> driveSubsystem.setParkMode(true)))
-        .onFalse(new InstantCommand(() -> driveSubsystem.setParkMode(false)));
-
-    driverController
-        .leftBumper()
-        .onTrue(new InstantCommand(() -> driveSubsystem.setFieldRelative(false)))
-        .onFalse(new InstantCommand(() -> driveSubsystem.setFieldRelative(true)));
-
-    driverController
-        .leftTrigger(0.1)
-        .onTrue(new InstantCommand(() -> driveSubsystem.setSpeedMode(SpeedMode.TURBO)))
-        .onFalse(new InstantCommand(() -> driveSubsystem.setSpeedMode((SpeedMode.NORMAL))));
-
-    driverController
-        .rightTrigger(0.1)
-        .onTrue(new InstantCommand(() -> driveSubsystem.setSpeedMode(SpeedMode.TURTLE)))
-        .onFalse(new InstantCommand(() -> driveSubsystem.setSpeedMode((SpeedMode.NORMAL))));
-  }
+  private void configureBindings() {}
 
   public void setTeleop(boolean mode) {
     isTeleop = mode;
