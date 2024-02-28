b90d2cc19e13e49468051416aa3829782bdff081
robertmakowsky <robmakowsky@icloud.com>
2024-02-27 19:06:05 -0700

    Update LightsSubsystem.java

--- src/main/java/frc/robot/subsystems/LightsSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightsSubsystem.java
@@ -5,29 +5,28 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.constants.LightsConstants;
 
 public class LightsSubsystem extends LightsConstants implements Subsystem {
-  private static String strip;
-  private static String color;
+  private static String Altstrip;
+  private static String Altcolor;
   private static byte[] data = new byte[10];
 
   public LightsSubsystem() {
-    super();
-    strip = "one";
-    color = "blank";
-    send();
+    Altstrip = "one";
+    Altcolor = "blank";
   }
-  public void massSignal(String c, String[] Strips){
-    for(int i = 0; i < data.length / 2; i++){
-      setAll(i ,Strips[i], c);
+
+  public void massSignal(String c, String[] Strips) {
+    for (int i = 0; i < data.length / 2; i++) {
+      setAll(i, Strips[i], c);
     }
     LightsConstants.communincation.writeBulk(data, data.length);
   }
 
   public void send() {
-    data[0] = (byte)((char)LightsConstants.strip.get(strip));
-    data[1] = (byte)((char)LightsConstants.color.get(color));
+    data[0] = (byte) ((char) LightsConstants.strip.get(Altstrip));
+    data[1] = (byte) ((char) LightsConstants.color.get(Altcolor));
     boolean failure = LightsConstants.communincation.writeBulk(data, data.length);
 
-    if (failure){
+    if (failure) {
       DataLogManager.log("led failure has occured");
     } else {
       LightsConstants.communincation.writeBulk(data, data.length);
@@ -35,21 +34,22 @@ public class LightsSubsystem extends LightsConstants implements Subsystem {
   }
 
   public void setColor(String c) {
-    color = c;
+    Altcolor = c;
     send();
   }
-  public void setAll(int index, String s, String c){
-    data[index * 2] = (byte)((char)LightsConstants.strip.get(s));
-    data[index + 1] = (byte)((char)LightsConstants.color.get(c));
+
+  public void setAll(int index, String s, String c) {
+    data[index * 2] = (byte) ((char) LightsConstants.strip.get(s));
+    data[index + 1] = (byte) ((char) LightsConstants.color.get(c));
   }
 
   public void setStrip(String s) {
-    strip = s;
+    Altstrip = s;
     send();
   }
 
-  public void all(){
-    data[0] = ((byte)(((char)LightsConstants.color.get(color))));
-    LightsConstants.communincation.writeBulk(data,data.length - 9);    
+  public void all() {
+    data[0] = ((byte) (((char) LightsConstants.color.get(Altcolor))));
+    LightsConstants.communincation.writeBulk(data, data.length - 9);
   }
 }
