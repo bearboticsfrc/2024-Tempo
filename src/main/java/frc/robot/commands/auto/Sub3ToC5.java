2965cbea314cbc725ee092e169a8ee9c50ca7c90
Kellen <70085049+Kellenn@users.noreply.github.com>
2024-03-15 19:38:21 -0600

    Refactor

--- src/main/java/frc/robot/commands/auto/Sub3ToC5.java
+++ src/main/java/frc/robot/commands/auto/Sub3ToC5.java
@@ -5,10 +5,8 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.commands.AutoAimCommand;
 import frc.robot.commands.AutoNotePickupCommand;
-import frc.robot.location.FieldPositions;
-import frc.robot.location.LocationHelper;
+import frc.robot.commands.AutoShootCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.manipulator.ManipulatorSubsystem;
 import frc.robot.subsystems.vision.ObjectDetectionSubsystem;
@@ -20,13 +18,7 @@ public class Sub3ToC5 {
       DriveSubsystem driveSubsystem,
       ManipulatorSubsystem manipulatorSubsystem,
       ObjectDetectionSubsystem objectDetectionSubsystem) {
-    return new AutoAimCommand(driveSubsystem, FieldPositions.getInstance().getSpeakerTranslation())
-        .andThen(
-            manipulatorSubsystem.getShooterPrepareCommad(
-                () ->
-                    LocationHelper.getDistanceToPose(
-                        driveSubsystem.getPose(), FieldPositions.getInstance().getSpeakerCenter())))
-        .andThen(manipulatorSubsystem.getShootCommand())
+    return new AutoShootCommand(driveSubsystem, manipulatorSubsystem)
         .andThen(new PathPlannerAuto("Sub3ToC5"))
         .andThen(
             new AutoNotePickupCommand(
@@ -35,17 +27,7 @@ public class Sub3ToC5 {
         .andThen(Commands.waitUntil(manipulatorSubsystem::isNoteInFeeder))
         .andThen(Commands.runOnce(() -> replanC5ToShootPath(driveSubsystem)))
         .andThen(AutoBuilder.followPath(c5ToShootPath))
-        .andThen(
-            new AutoAimCommand(
-                driveSubsystem, FieldPositions.getInstance().getSpeakerTranslation()))
-        .andThen(
-            manipulatorSubsystem
-                .getShooterPrepareCommad(
-                    () ->
-                        LocationHelper.getDistanceToPose(
-                            driveSubsystem.getPose(),
-                            FieldPositions.getInstance().getSpeakerCenter()))
-                .andThen(manipulatorSubsystem.getShootCommand()));
+        .andThen(new AutoShootCommand(driveSubsystem, manipulatorSubsystem));
   }
 
   private static void replanC5ToShootPath(DriveSubsystem driveSubsystem) {
