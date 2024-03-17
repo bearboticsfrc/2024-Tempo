057b52b3ae3e340ae4e2dd2fb06dd891496bc7ba
Ben Griffin <Bennett.Griffin@gmail.com>
2024-03-17 14:08:36 -0600

    Start on Sub2W2C3C2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,6 +31,7 @@ import frc.robot.commands.auto.MiddleC1;
 import frc.robot.commands.auto.MiddleC1C2;
 import frc.robot.commands.auto.MiddleTwoNote;
 import frc.robot.commands.auto.Sub1TwoNote;
+import frc.robot.commands.auto.Sub2W2C3C4;
 import frc.robot.commands.auto.Sub2W3W2W1C1;
 import frc.robot.commands.auto.Sub3ToC5C3;
 import frc.robot.commands.auto.Sub3TwoNote;
@@ -194,6 +195,10 @@ public class RobotContainer {
     autoCommandChooser.addOption(
         "7 - " + Sub3ToC5C3.NAME,
         Sub3ToC5C3.get(driveSubsystem, manipulatorSubsystem, objectDetectionSubsystem));
+
+    autoCommandChooser.addOption(
+        "8 - " + Sub2W2C3C4.NAME,
+        Sub2W2C3C4.get(driveSubsystem, objectDetectionSubsystem, manipulatorSubsystem));
 
     RobotConstants.COMPETITION_TAB
         .add("Auto Command", autoCommandChooser)
---
+++ src/main/java/frc/robot/commands/auto/Sub2W2C3C4.java
@@ -0,0 +1,93 @@
+package frc.robot.commands.auto;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.commands.AutoNotePickupCommand;
+import frc.robot.commands.AutoShootCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.manipulator.ManipulatorSubsystem;
+import frc.robot.subsystems.manipulator.ShooterSubsystem.ShooterVelocity;
+import frc.robot.subsystems.vision.ObjectDetectionSubsystem;
+
+public class Sub2W2C3C4 {
+  public static final String NAME = "Sub2W2C3C4";
+
+  static PathPlannerPath w2toc3Path = PathPlannerPath.fromPathFile("W2toC3");
+  static PathPlannerPath c3toShootPath = PathPlannerPath.fromPathFile("C3ToShoot");
+
+  static PathPlannerPath shootToc2Path = PathPlannerPath.fromPathFile("ShootToC2");
+
+  static PathPlannerPath c2toShootPath = PathPlannerPath.fromPathFile("C2ToShoot");
+
+  public static Command get(
+      DriveSubsystem driveSubsystem,
+      ObjectDetectionSubsystem objectDetectionSubsystem,
+      ManipulatorSubsystem manipulatorSubsystem) {
+    return manipulatorSubsystem
+        .getSubwooferShootCommand()
+        .andThen(
+            new PathPlannerAuto("Sub2W2")
+                .alongWith(
+                    manipulatorSubsystem.getShooterPrepareCommand(ShooterVelocity.PODIUM_SHOOT)))
+        .andThen(new WaitUntilCommand(manipulatorSubsystem::isNoteInFeeder))
+        .andThen(getAutoShootCommand(driveSubsystem, manipulatorSubsystem))
+        .andThen(
+            AutoBuilder.followPath(getReplannedW2ToC3Path(driveSubsystem)))
+        .andThen(
+            getAutoNotePickupCommand(
+                driveSubsystem, objectDetectionSubsystem, manipulatorSubsystem))
+        .andThen(
+            AutoBuilder.followPath(getReplannedC3ToShootPath(driveSubsystem))
+                .alongWith(
+                    manipulatorSubsystem.getShooterPrepareCommand(ShooterVelocity.PODIUM_SHOOT)))
+        .andThen(getAutoShootCommand(driveSubsystem, manipulatorSubsystem))
+        
+
+        .andThen(
+            AutoBuilder.followPath(getReplannedShootToC2Path(driveSubsystem)))
+        .andThen(
+            getAutoNotePickupCommand(
+                driveSubsystem, objectDetectionSubsystem, manipulatorSubsystem))
+        .andThen(
+            AutoBuilder.followPath(getReplannedC2ToShootPath(driveSubsystem))
+                .alongWith(
+                    manipulatorSubsystem.getShooterPrepareCommand(ShooterVelocity.PODIUM_SHOOT)))
+        .andThen(getAutoShootCommand(driveSubsystem, manipulatorSubsystem))
+        ;
+  }
+
+  private static Command getAutoShootCommand(
+      DriveSubsystem driveSubsystem, ManipulatorSubsystem manipulatorSubsystem) {
+    return new AutoShootCommand(driveSubsystem, manipulatorSubsystem);
+  }
+
+  private static PathPlannerPath getReplannedW2ToC3Path(DriveSubsystem driveSubsystem) {
+    return w2toc3Path.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static PathPlannerPath getReplannedC3ToShootPath(DriveSubsystem driveSubsystem) {
+    return c3toShootPath.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static PathPlannerPath getReplannedShootToC2Path(DriveSubsystem driveSubsystem) {
+    return shootToc2Path.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static PathPlannerPath getReplannedC2ToShootPath(DriveSubsystem driveSubsystem) {
+    return c2toShootPath.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static Command getAutoNotePickupCommand(
+      DriveSubsystem driveSubsystem,
+      ObjectDetectionSubsystem objectDetectionSubsystem,
+      ManipulatorSubsystem manipulatorSubsystem) {
+    return new AutoNotePickupCommand(
+            driveSubsystem,
+            objectDetectionSubsystem,
+            () -> manipulatorSubsystem.isNoteInFeeder() || manipulatorSubsystem.isNoteInRoller())
+        .alongWith(manipulatorSubsystem.getIntakeCommand());
+  }
+}
