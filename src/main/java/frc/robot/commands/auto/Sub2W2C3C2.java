703c8ed4bb109067d663ec00745863d6545897e3
Ben Griffin <Bennett.Griffin@gmail.com>
2024-03-19 22:39:50 -0600

    Yeah finally Sub2W2C3C2

---
+++ src/main/java/frc/robot/commands/auto/Sub2W2C3C2.java
@@ -0,0 +1,88 @@
+package frc.robot.commands.auto;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.commands.AutoNotePickupCommand;
+import frc.robot.commands.AutoShootCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.manipulator.ManipulatorSubsystem;
+import frc.robot.subsystems.manipulator.ShooterSubsystem.ShooterVelocity;
+import frc.robot.subsystems.vision.ObjectDetectionSubsystem;
+
+public class Sub2W2C3C2 {
+  private static final String AUTO_NAME = "Sub2W2";
+
+  public static final String NAME = "Sub2W2C3C2";
+
+  static PathPlannerPath w2toc3Path = PathPlannerPath.fromPathFile("W2toC3");
+  static PathPlannerPath c3toShootPath = PathPlannerPath.fromPathFile("C3ToShootVersion2");
+
+  static PathPlannerPath shootToc2Path = PathPlannerPath.fromPathFile("ShootToC2AfterC3");
+
+  static PathPlannerPath c2toShootPath = PathPlannerPath.fromPathFile("C2ToShootAfterC3");
+
+  public static Command get(
+      DriveSubsystem driveSubsystem,
+      ObjectDetectionSubsystem objectDetectionSubsystem,
+      ManipulatorSubsystem manipulatorSubsystem) {
+    return new PathPlannerAuto(AUTO_NAME)
+        .alongWith(manipulatorSubsystem.getLineShootCommand())
+        .andThen(new WaitUntilCommand(manipulatorSubsystem::isNoteInFeeder))
+        // .andThen(manipulatorSubsystem.getFarPodiumShootCommand())
+        .andThen(getAutoShootCommand(driveSubsystem, manipulatorSubsystem))
+        .andThen(AutoBuilder.followPath(getReplannedW2ToC3Path(driveSubsystem)))
+        .andThen(
+            getAutoNotePickupCommand(
+                driveSubsystem, objectDetectionSubsystem, manipulatorSubsystem))
+        .andThen(
+            AutoBuilder.followPath(getReplannedC3ToShootPath(driveSubsystem))
+                .alongWith(
+                    manipulatorSubsystem.getShooterPrepareCommand(ShooterVelocity.PODIUM_SHOOT)))
+        .andThen(getAutoShootCommand(driveSubsystem, manipulatorSubsystem))
+        .andThen(AutoBuilder.followPath(getReplannedShootToC2Path(driveSubsystem)))
+        .andThen(
+            getAutoNotePickupCommand(
+                driveSubsystem, objectDetectionSubsystem, manipulatorSubsystem))
+        .andThen(
+            AutoBuilder.followPath(c2toShootPath)
+                .alongWith(
+                    manipulatorSubsystem.getShooterPrepareCommand(ShooterVelocity.PODIUM_SHOOT)))
+        .andThen(getAutoShootCommand(driveSubsystem, manipulatorSubsystem))
+        .finallyDo(() -> manipulatorSubsystem.getShootStopCommand().schedule());
+  }
+
+  private static Command getAutoShootCommand(
+      DriveSubsystem driveSubsystem, ManipulatorSubsystem manipulatorSubsystem) {
+    return new AutoShootCommand(driveSubsystem, manipulatorSubsystem);
+  }
+
+  private static PathPlannerPath getReplannedW2ToC3Path(DriveSubsystem driveSubsystem) {
+    return w2toc3Path.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static PathPlannerPath getReplannedC3ToShootPath(DriveSubsystem driveSubsystem) {
+    return c3toShootPath.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static PathPlannerPath getReplannedShootToC2Path(DriveSubsystem driveSubsystem) {
+    return shootToc2Path.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static PathPlannerPath getReplannedC2ToShootPath(DriveSubsystem driveSubsystem) {
+    return c2toShootPath.replan(driveSubsystem.getPose(), driveSubsystem.getRobotRelativeSpeeds());
+  }
+
+  private static Command getAutoNotePickupCommand(
+      DriveSubsystem driveSubsystem,
+      ObjectDetectionSubsystem objectDetectionSubsystem,
+      ManipulatorSubsystem manipulatorSubsystem) {
+    return new AutoNotePickupCommand(
+            driveSubsystem,
+            objectDetectionSubsystem,
+            () -> manipulatorSubsystem.isNoteInFeeder() || manipulatorSubsystem.isNoteInRoller())
+        .alongWith(manipulatorSubsystem.getIntakeCommand());
+  }
+}
--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
@@ -213,6 +213,14 @@ public class ManipulatorSubsystem extends SubsystemBase {
     return getShootCommand(ArmPosition.PODIUM_SHOOT, ShooterVelocity.PODIUM_SHOOT);
   }
 
+  public Command getFarPodiumShootCommand() {
+    return getShootCommand(ArmPosition.FAR_PODIUM_SHOOT, ShooterVelocity.PODIUM_SHOOT);
+  }
+
+  public Command getFarLineShootCommand() {
+    return getShootCommand(ArmPosition.FAR_LINE_SHOOT, ShooterVelocity.SUBWOOFER_SHOOT);
+  }
+
   public Command getLineShootCommand() {
     return getShootCommand(ArmPosition.LINE_SHOOT, ShooterVelocity.SUBWOOFER_SHOOT);
   }
